5b022e4116df3e6792b8cebf480e30a43565ab9d
leen.zhou.cn <leen.zhou.cn@gmail.com>
2022-01-25 16:39:37 +0000

    bindsnoop command

--- bindsnoop/src/main.rs
+++ bindsnoop/src/main.rs
@@ -5,13 +5,41 @@ use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 struct Command {
-    
+    /// Process ID to trace
+    #[structopt(short = "p", default_value = "0", value_name = "PID")]
+    pid: i32,
+    /// Include errors on output.
+    #[structopt(short = "x")]
+    fails: bool,
+    /// Comma-separated list of ports to trace.
+    #[structopt(short = "P", default_value = "80", use_delimiter = true)]
+    port: Vec<u32>,
 }
 
 fn main() -> Result<()> {
+    let opts: Command = Command::from_args();
+
     let skel_builder = BindsnoopSkelBuilder::default();
-    let open_skel = skel_builder.open()?;
+    let mut open_skel = skel_builder.open()?;
+    open_skel.rodata().target_pid = opts.pid;
+    open_skel.rodata().ignore_errors = opts.fails;
+    open_skel.rodata().filter_by_port = !opts.port.is_empty();
+
     let mut skel = open_skel.load()?;
+
+    if !opts.port.is_empty() {
+        /* TODO:
+            if (target_ports) {
+            port_map_fd = bpf_map__fd(obj->maps.ports);
+            port = strtok(target_ports, ",");
+            while (port) {
+                port_num = strtol(port, NULL, 10);
+                bpf_map_update_elem(port_map_fd, &port_num, &port_num, BPF_ANY);
+                port = strtok(NULL, ",");
+            }
+        }
+             */
+    }
     skel.attach()?;
     Ok(())
 }
