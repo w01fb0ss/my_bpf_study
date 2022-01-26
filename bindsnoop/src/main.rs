0a5fa675b2d35e95a1c739a4e05290d34f58c77d
w01fb0ss <leen.zhou.cn@gmail.com>
2022-01-26 02:49:43 +0000

    bindsnoop PerfBufferBuilder

--- bindsnoop/src/main.rs
+++ bindsnoop/src/main.rs
@@ -1,6 +1,8 @@
 mod bpf;
 use anyhow::Result;
 use bpf::*;
+use libbpf_rs::{MapFlags, PerfBufferBuilder};
+use std::time::Duration;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -12,8 +14,8 @@ struct Command {
     #[structopt(short = "x")]
     fails: bool,
     /// Comma-separated list of ports to trace.
-    #[structopt(short = "P", default_value = "80", use_delimiter = true)]
-    port: Vec<u32>,
+    #[structopt(short = "P", use_delimiter = true)]
+    port: Vec<u16>,
 }
 
 fn main() -> Result<()> {
@@ -26,12 +28,17 @@ fn main() -> Result<()> {
     open_skel.rodata().filter_by_port = !opts.port.is_empty();
 
     let mut skel = open_skel.load()?;
-    let mut maps = skel.maps_mut();
-    let map = maps.ports();
 
     if !opts.port.is_empty() {
+        for (i, value) in opts.port.iter().enumerate() {
+            let key = i.to_ne_bytes();
+            let value = value.to_ne_bytes();
+            skel.maps_mut()
+                .ports()
+                .update(&key, &value, MapFlags::ANY)?;
+        }
 
-        /* TODO:
+        /*
             map.update(key, value, flags)
             if (target_ports) {
             port_map_fd = bpf_map__fd(obj->maps.ports);
@@ -42,8 +49,23 @@ fn main() -> Result<()> {
                 port = strtok(NULL, ",");
             }
         }
-             */
+        */
     }
     skel.attach()?;
-    Ok(())
+
+    let perf = PerfBufferBuilder::new(skel.maps().events())
+        .sample_cb(handle_event)
+        .lost_cb(handle_lost_events)
+        .build()?;
+    loop {
+        perf.poll(Duration::from_millis(100))?;
+    }
+}
+
+fn handle_event(_cpu: i32, data: &[u8]) {
+    
+}
+
+fn handle_lost_events(cpu: i32, count: u64) {
+    eprintln!("Lost {} events on CPU {}", count, cpu);
 }
