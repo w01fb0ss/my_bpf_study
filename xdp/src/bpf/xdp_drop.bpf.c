4334e04f5b14640addb0e6a64283a1f08b325b1f
w01fb0ss <leen.zhou.cn@gmail.com>
2022-03-25 08:22:53 +0000

    用vmlinux.h屏蔽访问linux.h

--- xdp/src/bpf/xdp_drop.bpf.c
+++ xdp/src/bpf/xdp_drop.bpf.c
@@ -1,36 +1,31 @@
-#include <stdbool.h>
-#include <linux/bpf.h>
+#include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
-#include <bpf/bpf_endian.h>
 #include <bpf/bpf_tracing.h>
-#include <linux/in.h>
-#include <linux/if_ether.h>
-#include <linux/ip.h>
 
-static bool is_ICMP(void *data_begin, void *data_end)
-{
-    // sudo cat /sys/kernel/debug/tracing/trace_pipe
-    // bpf_printk("Entering is_ICMP\n");
-    struct ethhdr *eth = data_begin;
-
-    if ((void *)(eth + 1) > data_end)
-        return false;
-
-    if (eth->h_proto == bpf_htons(ETH_P_IP))
-    {
-        struct iphdr *iph = (struct iphdr *)(eth + 1);
-        if ((void *)(iph + 1) > data_end)
-            return false;
-
-        if (iph->protocol == IPPROTO_ICMP)
-            return true;
-    }
-    else if (eth->h_proto == bpf_htons(ETH_P_8021Q))
-    {
-        return false;
-    }
-    return false;
-}
+// static bool is_ICMP(void *data_begin, void *data_end)
+// {
+//     // sudo cat /sys/kernel/debug/tracing/trace_pipe
+//     // bpf_printk("Entering is_ICMP\n");
+//     struct ethhdr *eth = data_begin;
+// 
+//     if ((void *)(eth + 1) > data_end)
+//         return false;
+// 
+//     if (eth->h_proto == bpf_htons(ETH_P_IP))
+//     {
+//         struct iphdr *iph = (struct iphdr *)(eth + 1);
+//         if ((void *)(iph + 1) > data_end)
+//             return false;
+// 
+//         if (iph->protocol == IPPROTO_ICMP)
+//             return true;
+//     }
+//     else if (eth->h_proto == bpf_htons(ETH_P_8021Q))
+//     {
+//         return false;
+//     }
+//     return false;
+// }
 
 SEC("xdp")
 int xdp_drop_icmp(struct xdp_md *ctx)
@@ -39,8 +34,17 @@ int xdp_drop_icmp(struct xdp_md *ctx)
     void *data_end = (void *)(long)ctx->data_end;
     void *data = (void *)(long)ctx->data;
 
-    if (is_ICMP(data, data_end))
-        return XDP_DROP;
+// if (is_ICMP(data, data_end))
+//       return XDP_DROP;
+
+    struct iphdr *ip = data + sizeof(struct ethhdr);
+    if ((void *)(ip + 1) > data_end)
+		return XDP_PASS;
+    if (ip->protocol == IPPROTO_ICMP)
+    {
+        // bpf_printk("PING");
+	return XDP_DROP;
+    }
 
     return XDP_PASS;
 }
